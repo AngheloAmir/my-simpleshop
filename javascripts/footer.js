f173fdcb157adf5cc9dcf3b6fef53bdc355ff381
Anghelo Amir <angheloamir@gmail.com>
2022-01-29 00:13:40 +0800

    updated, remove other names

--- javascripts/footer.js
+++ javascripts/footer.js
@@ -1,7 +1,7 @@
 document.getElementById('footer').innerHTML = `
 <div style="background-color: #a2b0c2; padding: 2rem 0;">
     <h3 style="text-align: center; font-size: 1rem; width: 80%; margin-left: 10%;">
-        Tap Tap Shop by Amir, Casubha, Dionisio, Libre</h3>
+        Tap Tap Shop by Amir</h3>
     <h3 style="text-align: center; font-size: 1rem; width: 80%; margin-left: 10%;">
         For EDUCATIONAL and DEMOSTRATION PURPOSE ONLY - School Project Finals</h3>
 </div>
